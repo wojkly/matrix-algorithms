2153f88aab782f09ed84d717a64aca5be94b3e79
yauza <jan.stobnicki@risco.net.pl>
2021-11-11 23:40:47 +0100

    :sunglasses:

--- lab2/gen_matrices.py
+++ lab2/gen_matrices.py
@@ -4,14 +4,14 @@ oc = Oct2Py()
 
 #gen fems
 
-for i in range(2,20):
-    filename = "matrices/fem_" + str(i)  + ".csv"
-    oc.gen_mat(1,i,2,0, filename)
-    oc.restart()
-    print("file " + filename + "done")
+#for i in range(2,20):
+#    filename = "matrices/fem_" + str(i)  + ".csv"
+#    oc.gen_mat(1,i,2,0, filename)
+#    oc.restart()
+#    print("file " + filename + "done")
 
 #gen igas
-for i in range(2,20):
+for i in range(9,20):
     filename = "matrices/iga_" + str(i)  + ".csv"
     oc.gen_mat(0,i,2,0, filename)
     oc.restart()
